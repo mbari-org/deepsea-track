326e2dfb2354c4246f7c69a71ec439a476062627
Danelle Cline <dcline@mbari.org>
2020-11-18 09:23:40 -0800

    feat: added all arguments

--- include/deepsea/arguments.h
+++ include/deepsea/arguments.h
@@ -19,6 +19,7 @@
  */
 //!\file options.h  Manages parsing the command line
 
+#include <boost/program_options.hpp>
 #include <iostream>
 
 using namespace std;
@@ -32,17 +33,29 @@ namespace deepsea {
         /// \brief Constructor
         Arguments(int argc, char **argv);
 
+        bool detection_options(const boost::program_options::variables_map & vm);
+
+        inline bool hasXml() {
+            if (xml_path_.length() > 0)
+                return true;
+            return false;
+        };
+
         virtual ~Arguments();
 
-        ///
-        //" Usage: deepsea-track <path to video> <path to xml> <start frame num> <frame resize width> < frame resize height> <stride(optional)>\n"
-        //" examples:\n"
-        string getVideoPath();
+        bool initialized_;
 
-    private:
-        string video_path_;               ///! absolute path to the video file to process
-        unsigned int start_frame_num_;    ///! starting frame; used only during startup
-        string out_dir_;                    ///! output directory to save all output to
+        string video_name_;              ///! name of the video file to process
+        string xml_path_;                ///! absolute path to directory with voc xml files
+        string out_path_;                ///! absolute path to save any output artifacts
+        unsigned int start_frame_num_;   ///! starting frame; used only during startup
+        string in_path_;                 ///! input path for the video file, deepsea_class_map.json and deepsea_cfg.json files
+        unsigned int resize_width_;      ///! resize width in pixels for resizing the video before running the tracker
+        unsigned int resize_height_;     ///! resize hight in pixels for resizing the video before running the tracker
+        unsigned int stride_;            ///! stride in detections, 1-based. e.g. --stride=5 will skip seeding new events to every 5th detection
+        string topic_;                   ///! topic to listen on at address, e.g. VisualEvents
+        string address_;                 ///! socket address for the detector output, e.g. tcp://127.0.0.1:6432
     };
+
 }
 
--- src/arguments.cc
+++ src/arguments.cc
@@ -1,4 +1,3 @@
-#pragma once
 /*
  * Copyright 2020 MBARI
  *
@@ -20,6 +19,7 @@
 //!\file Arguments.h  Manages parsing the command line Arguments
 
 #include <iostream>
+#include <sys/stat.h>
 #include <boost/program_options.hpp>
 #include <deepsea/arguments.h>
 
@@ -31,34 +31,91 @@ namespace deepsea {
 // ######################################################################
     Arguments::Arguments(int argc, char **argv){
 
+        initialized_ = false;
         po::options_description desc("Allowed Arguments");
         desc.add_options()
                 ("help", "produce help message")
-                ("start_frame_num", po::value<unsigned int>(&start_frame_num_)->default_value(1), "starting frame to process, 1-based. e.g. --start_frame=1 is the first frame in the video")
-                ("video_path", po::value<string>(&video_path_), "absolute path to the video file to process")
+                ("start_frame_num", po::value<unsigned int>(&start_frame_num_)->default_value(1),
+                        "starting frame to process, 1-based. e.g. --start_frame=1 is the first frame in the video")
+                ("resize_width", po::value<unsigned int>(&resize_width_)->default_value(512),
+                        "resize width in pixels for running the tracker")
+                ("resize_height", po::value<unsigned int>(&resize_height_)->default_value(512),
+                        "resize width in pixels for running the tracker")
+                ("stride", po::value<unsigned int>(&stride_)->default_value(1),
+                        "stride in detections, 1-based. e.g. --stride=5 will skip ip seeding new events to every 5th detection")
+                ("video_name", po::value<string>(&video_name_),
+                        "name of the video file to process")
+                ("out_path", po::value<string>(&out_path_),
+                 "absolute path to save all output to")
+                ("in_path", po::value<string>(&in_path_),
+                        "absolute path to the input artifacts: video.mov, deepsea_class_map.json and deepsea_cfg.jsonfiles")
+                ("xml_path", po::value<string>(&xml_path_),
+                 "absolute path to directory with voc xml files. If absent, --address and --topic must be set")
+                ("address", po::value<string>(&address_),
+                        "socket address for the detector output, e.g. tcp://127.0.0.1:6432")
+                ("topic", po::value<string>(&topic_),
+                        "topic to listen on at address, e.g. VisualEvents ")
                 ;
 
         po::variables_map vm;
         po::store(po::parse_command_line(argc, argv, desc), vm);
         po::notify(vm);
+        struct stat buffer;
 
         if (vm.count("help")) {
             cout << desc << "\n";
             return;
         }
+        bool failure = this->detection_options(vm);
 
-        //<path to video> <path to xml> <start frame num> <frame resize width> < frame resize height> <stride(optional)>
-        if (vm.count("video_path")) {
-            cout << "Video to process was set to " << video_path_ << endl;
-            //TODO: check video_path existance
+        if (vm.count("in_path")) {
+            if (stat (in_path_.c_str(), &buffer) != 0) {
+                cout << "Directory " << in_path_ << " does not exist" << endl;
+                failure = true;
+            }
+            else {
+                cout << "Input path was set to " << in_path_ << endl;
+            }
+        }
+        else {
+            cout << "Input path must be set with --in_path" << endl;
+            failure = true;
+        }
+        if (vm.count("video_name")) {
+            string video_path =  this->in_path_ + "/" + this->video_name_;
+            if (stat (video_path.c_str(), &buffer) != 0) {
+                cout << video_path << " does not exist";
+                failure = true;
+            }
+            else {
+                cout << "Video to process was set to " << video_path << endl;
+            }
         } else {
-            cout << "Must set the video_path with --video_path" << endl;
-            return;
+            cout << "Must set the video_name with --video_name and input with --input_path" << endl;
+            failure = true;
         }
+        if (vm.count("out_path")) {
+            if (stat (out_path_.c_str(), &buffer) != 0) {
+                cout << "Directory " << out_path_ << " does not exist" << endl;
+                failure = true;
+            }
+            else {
+                cout << "Output path to log files was set to " << out_path_ << endl;
+            }
+        }
+        if (!vm.count("start_frame_num")) {
+            start_frame_num_ = 1;
+        }
+        cout << "Start processing frame " << start_frame_num_ << endl;
 
-        if (vm.count("start_frame_num")) {
-            cout << "Start processing frame " << start_frame_num_ << endl;
+        if (vm.count("resize_width") && vm.count("resize_height")) {
+            cout << "Resizing to " << resize_width_ << "x" << resize_height_ << endl;
         }
+        if (vm.count("stride")) {
+            cout << "Skipping every " << stride_ << " detection" << endl;
+        }
+        if (!failure)
+            initialized_ = true;
     }
 
 // ######################################################################
@@ -66,8 +123,29 @@ namespace deepsea {
     {
     }
 
-// ######################################################################
-    string Arguments::getVideoPath() {
-        return video_path_;
+    bool Arguments::detection_options(const boost::program_options::variables_map & vm)
+    {
+        bool failure = false;
+        struct stat buffer;
+
+        if (vm.count("xml_path") && (vm.count("address") || vm.count("topic") ))  {
+            throw std::logic_error(std::string("Specify --topic and --address or detection input from --xml_path"));
+        }
+
+        if (vm.count("xml_path")) {
+            if (stat (xml_path_.c_str(), &buffer) != 0) {
+                cout << "Directory " << xml_path_ << " does not exist" << endl;
+                failure = true;
+            }
+            else {
+                cout << "Input path with xml detections was set to " << xml_path_ << endl;
+            }
+        }
+        if (vm.count("topic") && vm.count("address")) {
+            cout << "Listening for topic " << topic_ << " at " << address_ << endl;
+        }
+        return failure;
+
     }
+
 }
