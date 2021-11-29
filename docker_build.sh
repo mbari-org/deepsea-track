f4a2ed4e81b5289898d34e24ae1f8b96a2fc1745
danellecline <dcline@mbari.org>
2021-11-29 14:08:22 -0800

    build: correc image name for amd64 build

--- docker_build.sh
+++ docker_build.sh
@@ -7,4 +7,4 @@ docker image push mbari/deepsea-track:ubuntu20.04-cuda11.3.1-arm64
 
 echo "Building deepsea-track for amd64, cuda 11.3.1, ubuntu20.04"
 docker buildx bake -f docker-compose.yml --load --progress=plain amd64
-docker image push mbari/deepsea-track:ubuntu20.04-cuda11.3.1
+docker image push mbari/deepsea-track:ubuntu20.04-cuda11.3.1-amd64
