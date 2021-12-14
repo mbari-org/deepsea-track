8bbfd08dd208cd7a7e18bbaff260d34a9d56c6a2
danellecline <dcline@mbari.org>
2021-12-14 12:38:12 -0800

    build: added version to docker releases

--- docker_build.sh
+++ docker_build.sh
@@ -1,10 +1,16 @@
 #! /bin/sh
+VERSION=`cat VERSION.txt`
+
 docker buildx use deepsea-track-docker-builder
 
 echo "Building deepsea-track for arm64, cuda 11.3.1, ubuntu20.04"
 docker buildx bake -f docker-compose.yml --load --progress=plain arm64
 docker image push mbari/deepsea-track:ubuntu20.04-cuda11.3.1-arm64
+docker tag mbari/deepsea-track:ubuntu20.04-cuda11.3.1-arm64
+docker push mbari/deepsea-track:ubuntu20.04-cuda11.3.1-arm64-v${VERSION}
 
 echo "Building deepsea-track for amd64, cuda 11.3.1, ubuntu20.04"
 docker buildx bake -f docker-compose.yml --load --progress=plain amd64
 docker image push mbari/deepsea-track:ubuntu20.04-cuda11.3.1-amd64
+docker tag mbari/deepsea-track:ubuntu20.04-cuda11.3.1-amd64 mbari/deepsea-track:ubuntu20.04-cuda11.3.1-amd64-v${VERSION}
+docker push mbari/deepsea-track:ubuntu20.04-cuda11.3.1-amd64-v${VERSION}
